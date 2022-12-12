d8d7ae58ffd56e48b322c1e13a9f7e6810a025ff
mhmdfakhrim <muhfakhri4@gmail.com>
2022-12-12 16:40:40 +0700

    latihan

--- Program.cs
+++ Program.cs
@@ -9,7 +9,25 @@ namespace Queues_144
 
     class Program
     {
-        int F
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+
+        public Program()
+        {
+            /*Initializing the values of the variables REAR and FRONT to -1 to indicate that
+             * the queue is initially empty. */
+            FRONT = -1;
+            REAR = -1;  
+        }
+
+        public void insert(int element)
+        {
+            /*this statement checks for the overflow condition. */
+            if ( FRONT == 0 && REAR == max - 1 ) || ( FRONT == REAR +11)) 
+                    {
+
+            }
+        }
         static void Main(string[] args)
         {
         }
