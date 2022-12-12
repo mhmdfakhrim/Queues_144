3e107b607b144f5625d516d2ebdaba892bc49137
mhmdfakhrim <muhfakhri4@gmail.com>
2022-12-12 16:32:33 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues_144
+{
+
+    class Program
+    {
+        int F
+        static void Main(string[] args)
+        {
+        }
+    }
+}
